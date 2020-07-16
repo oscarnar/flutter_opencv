c191d4994ef211979134b9011c59e0928f274305
oscarnar <orosas58@gmail.com>
2020-07-16 01:03:44 -0500

    Change sort

--- android/src/main/java/com/mulgundkar/opencv/core/CVCore.java
+++ android/src/main/java/com/mulgundkar/opencv/core/CVCore.java
@@ -56,7 +56,7 @@ public class CVCore {
                 }
             });
             double maxArea = 10;
-            for(int i=contours.size()-1; i>=0; i--){
+            for(int i=0; contours.size() > i; i++){
                 MatOfPoint cont = contours.get(i);
                 MatOfPoint2f pf = new MatOfPoint2f(cont.toArray());
                 MatOfPoint2f aprox = new MatOfPoint2f();
@@ -73,7 +73,6 @@ public class CVCore {
                         }
                         maxArea = tempArea;
                     }
-                    break;
                 }
                 points = null;
                 pf = null;
@@ -109,18 +108,9 @@ public class CVCore {
             Imgproc.Canny(grayMat, cannyEdges, 10, 50);
 
             Imgproc.findContours(cannyEdges, contours, hierarchy, Imgproc.RETR_LIST,Imgproc.CHAIN_APPROX_SIMPLE);
-            contours.sort(new Comparator<MatOfPoint>() {
-                @Override
-                public int compare(MatOfPoint o1, MatOfPoint o2) {
-                    double r1 = Imgproc.contourArea(o1);
-    
-                    double r2 = Imgproc.contourArea(o2);
-    
-                    return (int) (r1 - r2);
-                }
-            });
+            
             double maxArea = 0;
-            for(int i=contours.size()-1; i>=0; i--){
+            for(int i=0; contours.size() > i; i++){
                 MatOfPoint cont = contours.get(i);
                 MatOfPoint2f pf = new MatOfPoint2f(cont.toArray());
                 MatOfPoint2f aprox = new MatOfPoint2f();
@@ -137,7 +127,6 @@ public class CVCore {
                         }
                         maxArea = tempArea;
                     }
-                    break;
                 }
                 points = null;
                 pf = null;
